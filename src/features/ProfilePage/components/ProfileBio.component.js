cc564bb0201f8b3d6945728bcf84c72c8a37d3c7
Bartłomiej Marzec <bartekma89@gmail.com>
2019-12-18 00:04:03 +0100

    refactored

--- src/features/ProfilePage/components/ProfileBio.component.js
+++ src/features/ProfilePage/components/ProfileBio.component.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { string } from 'prop-types';
-import { isNil } from 'lodash';
+import { isEmpty } from 'lodash';
 import { Card, CardTitle, CardText } from 'reactstrap';
 
-const ProfileBio = ({ bio }) => {
-  return (
-    <>
-      {!isNil(bio) ? (
-        <Card className="my-2 cardbox" body>
-          <CardTitle>
-            <h3 className="heading">Bio</h3>
-          </CardTitle>
-          <CardText>{bio}</CardText>
-        </Card>
-      ) : null}
-    </>
-  );
-};
+const ProfileBio = ({ bio }) => (
+  <>
+    {!isEmpty(bio) ? (
+      <Card className="my-2 cardbox" body>
+        <CardTitle>
+          <h3 className="heading">Bio</h3>
+        </CardTitle>
+        <CardText>{bio}</CardText>
+      </Card>
+    ) : null}
+  </>
+);
 
 ProfileBio.propTypes = {
   bio: string
